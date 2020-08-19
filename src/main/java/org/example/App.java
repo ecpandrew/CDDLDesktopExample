6f0dbe5ef0376dc8d9c9310210c5b590edfa47f7
andre <andreluizalmeidacardoso@gmail.com>
2020-08-19 03:58:21 -0300

    Commit inicial

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import br.ufma.lsdi.cddl.CDDL;
+import br.ufma.lsdi.cddl.Connection;
+import br.ufma.lsdi.cddl.ConnectionFactory;
+import br.ufma.lsdi.cddl.message.Message;
+import br.ufma.lsdi.cddl.pubsub.Publisher;
+import br.ufma.lsdi.cddl.pubsub.PublisherFactory;
+import br.ufma.lsdi.cddl.pubsub.Subscriber;
+import br.ufma.lsdi.cddl.pubsub.SubscriberFactory;
+import br.ufma.lsdi.security.SecurityServiceImpl;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+
+
+
+    }
+}
